fc6ac5b9e34d313d4673fb7814009553fbda86d4
0xRy4n <ry4n@hackthebox.eu>
2023-07-25 14:01:25 +0300

    Added schemas import interface

---
+++ intercom_python_sdk/schemas/__init__.py
@@ -0,0 +1,24 @@
+from ..apis.admins.schemas import (
+    AdminSchema, 
+    AdminListSchema, 
+    TeamPriorityLevelSchema
+)
+
+from ..apis.articles.schemas import (
+    ArticleSchema, 
+    ArticleListSchema, 
+    ArticleStatisticsSchema, 
+    CreateArticleSchema,
+    DeletedArticleSchema
+)
+
+from ..apis.data_attributes.schemas import (
+    DataAttributeSchema,
+    DataAttributeListSchema,
+)
+
+from ..apis.data_events.schemas import (
+    DataEventSchema,
+    DataEventsListSchema,
+    DataEventSummarySchema
+)
