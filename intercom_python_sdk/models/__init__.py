7d85635a7ef2846aa5dc0eb096f0325d405117cc
0xRy4n <ry4n@hackthebox.eu>
2023-07-25 14:01:16 +0300

    Added models import interface

---
+++ intercom_python_sdk/models/__init__.py
@@ -0,0 +1,21 @@
+from ..apis.admins.models import (
+    Admin, 
+    AdminList
+)
+
+from ..apis.articles.models import (
+    Article, 
+    ArticleList
+)
+
+from ..apis.data_attributes.models import (
+    DataAttribute, 
+    DataAttributeList
+)
+
+from ..apis.data_events.models import (
+    DataEvent, 
+    DataEventsList
+)
+
+from ..apis.data_export.models import DataExportJob
