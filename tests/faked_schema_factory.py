def7b93b19ae400032420b9a8551725b13186f04
0xRy4n <ry4n@hackthebox.eu>
2023-07-28 04:26:34 +0300

    Created class to generate fake data automatically for a Marshmallow schema using Faker

--- tests/faked_schema_factory.py
+++ tests/faked_schema_factory.py
@@ -10,7 +10,7 @@ from marshmallow import Schema, fields
 
 
 class FakedSchemaFactory:
-    def __init__(self, seed=1337):
+    def __init__(self):
         self.fake = faker.Faker()
 
         self.schema_map = {
@@ -28,19 +28,22 @@ class FakedSchemaFactory:
         }
 
     def _fake_list(self, field: fields.List):
-        def fake_list():
-            return [
-                self.schema_map[field.inner.__class__]() # noqa # type: ignore
-                for _ in range(self.fake.random_int(max=10))
-            ]
-        return fake_list
-        
+            def fake_list():
+                inner = field.inner
+
+                if isinstance(inner, fields.Nested):
+                    list_val = lambda: self.fake_schema(inner.nested)[1]  # noqa # type: ignore
+                else:
+                    list_val = self.schema_map[inner.__class__]  # noqa # type: ignore
+
+                return [list_val() for _ in range(self.fake.random_int(min=1, max=10))]
+
+            return fake_list
+
     def _fake_tuple(self, field: fields.Tuple):
         def fake_tuple():
-            return tuple(
-                self.schema_map[field.inner.__class__]() # noqa # type: ignore
-                for _ in range(self.fake.random_int(max=10))
-            )
+            return tuple(self._fake_list(field)())  # noqa # type: ignore
+
         return fake_tuple
 
     def _get_faker_method_by_attr_name(self, attr_name: str) -> Optional[Callable]:
@@ -55,7 +58,13 @@ class FakedSchemaFactory:
             return self.schema_map[field.__class__]
 
         elif isinstance(field, fields.Nested):
-            return lambda: self.fake_schema(field.nested) # noqa # type: ignore
+            if field.many:
+                return lambda: [
+                    self.fake_schema(field.nested)[1]
+                    for _ in range(self.fake.random_int(min=1, max=10))
+                ]
+            else:
+                return lambda: self.fake_schema(field.nested)  # noqa # type: ignore
 
         elif isinstance(field, fields.List):
             return self._fake_list(field)
@@ -64,11 +73,13 @@ class FakedSchemaFactory:
             return self._fake_tuple(field)
 
         elif isinstance(field, fields.Dict):
-            if value_type := field.value_field:
-                return lambda: {self.fake.pystr(): self.fake_schema(value_type)} # noqa # type: ignore
-            else:
+            if not (value_type := field.value_field):
                 return self.fake.pydict
 
+            if isinstance(value_type, fields.Nested):
+                return lambda: {self.fake.pystr(): self.fake_schema(value_type)}  # noqa # type: ignore
+            else:
+                return lambda: {self.fake.pystr(): self.schema_map[value_type.__class__]()}  # noqa # type: ignore
         else:
             raise NotImplementedError(f"Cannot fake field: {field}")
 
@@ -79,14 +90,19 @@ class FakedSchemaFactory:
         for field_name, field in schema_obj.fields.items():
             if field.dump_default:
                 setattr(schema_obj, field_name, field.dump_default)
-                data[field_name] = field.dump_default
+                faker_val = field.dump_default
             else:
                 faker_method = self.get_faker_method_for_field(field)
                 setattr(schema_obj, field_name, faker_method())
-                
-                faker_val = faker_method()
-                if isinstance(faker_val, tuple):
-                    faker_val = faker_val[-1]
-                data[field_name] = faker_val
 
+                faker_val = faker_method()
+                faker_val = self._get_last_of_tuple(faker_val)
+
+                if isinstance(faker_val, list):
+                    faker_val = [self._get_last_of_tuple(val) for val in faker_val]
+
+                data[field_name] = faker_val
         return schema_obj, data
+
+    def _get_last_of_tuple(self, val):
+        return self._get_last_of_tuple(val[-1]) if isinstance(val, tuple) else val
